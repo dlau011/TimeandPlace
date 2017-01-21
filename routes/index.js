0e9ad6ce8de3959511cadf0836007466541b2022
dlau011 <dlau011@gmail.com>
2017-01-21 15:38:12 -0500

    for u macchew

--- routes/index.js
+++ routes/index.js
@@ -8,19 +8,45 @@ MongoClient.connect('mongodb://localhost:27017/TimeandPlace', function (err, con
         console.log(err.message);
         throw new Error(err);
     } else {
-        console.log("connected to server");
+        console.log("connected to server index.js");
         db = conn;
     }
 });
-
-/* GET home page. */
-router.get('/', function (req, res, next) {
-
-    db.collection('userdef').findOne({name:'Denise'}, function (err, result) {
-        console.log(result);
-        res.render('index', {title: 'Your UserID is ' + result.userid});
+function getNextSequenceValue(sequenceName) {
+    var sequenceDocument = db.collection('counters').updateOne({
+        query:{_id: sequenceName },
+        update: {$inc:{sequence_value:1}},
+        new:true
     });
 
+    return sequenceDocument.sequence_value;
+}
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    console.log("router.get /");
+    res.render('index', {title: 'router.get title'});
 });
 
+// called when user clicks Create Poll
+router.post('/createpoll', function (req, res) {
+    var polldef = {
+        pollid: getNextSequenceValue('pollid'),
+        groupdef: {
+            groupname : req.body.groupname,
+            groupmembers : [
+                {username : req.body.username,  userid: getNextSequenceValue('userid')}
+            ]
+        },
+        postalcode: req.body.postalcode,
+        activities: [],
+        times: []
+    }
+    db.collection('polldef').insert(polldef, function(err, res) {
+        if (err) {
+            console.log("error inserting polldef");
+        } else {
+            console.log("Inserted a document into the polldef collection.");
+        }
+    });
+});
 module.exports = router;
